40b62332ac1c577987c4c87e5c08ef7aaafbbc5b
ONYIBEFAITHEKUMA <onyibefaithekuma@gmail.com>
2024-07-10 12:31:17 +0100

    sceond commit

--- script.js
+++ script.js
@@ -43,36 +43,3 @@ function appendToDisplay(value) {
 
 
 
-
-// // app.js
-// function Calculator() {
-//     const [displayValue, setDisplayValue] = React.useState('');
-
-//     const handleButtonClick = (value) => {
-//         if (value === 'C') {
-//             setDisplayValue('');
-//         } else if (value === 'DEL') {
-//             setDisplayValue(displayValue.slice(0, -1));
-//         } else if (value === '=') {
-//             try {
-//                 setDisplayValue(eval(displayValue).toString());
-//             } catch {
-//                 setDisplayValue('Error');
-//             }
-//         } else {
-//             setDisplayValue(displayValue + value);
-//         }
-//     };
-
-//     return (
-//         <div className="calculator">
-//             <input type="text" id="display" value={displayValue} disabled />
-//             <div className="buttons">
-//                 {['C', 'DEL', '/', '*', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '=', '0', '.'].map(button => (
-//                     <button key={button} className="btn" onClick={() => handleButtonClick(button)}>{button}</button>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// }
-
